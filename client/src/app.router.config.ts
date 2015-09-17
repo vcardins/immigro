b03ee1d630eebfa9a08057663cf241552828d4e0
Victor Cardins <victor@results.com>
2015-09-17 01:24:57 -0400

    Routes config cleanup

--- client/src/app.router.config.ts
+++ client/src/app.router.config.ts
@@ -22,16 +22,16 @@ export default class {
 				{ route: 'flickr',         name: 'flickr',       moduleId: 'modules/flickr/flickr',             nav: true, title:'Flickr', access: accessLevels.user, icon:'image', description:'' },
 				//{ route: 'maps',           name: 'maps',         moduleId: './modules/maps/maps',                 nav: true, title:'Maps', auth:true, icon:'map-o', description:'Google Maps' },
 				{ route: 'child-router',   name: 'child-router', moduleId: './modules/child-router/child-router', nav: true, title:'Child Router', access: accessLevels.user, icon:'router', description:'' },
-				{ route: 'links',   	   name: 'links', 		 moduleId: 'modules/links/links', 			  nav: true, title:'Links', access: accessLevels.user, icon:'link', description:'' },
+				{ route: 'links',   	   name: 'links', 		   moduleId: 'modules/links/links', 			  nav: true, title:'Links', access: accessLevels.user, icon:'link', description:'' },
 				{ route: 'contacts',  	   name: 'contacts', 	 moduleId: './modules/contacts/contacts', 	      nav: true, title:'Contacts', access: accessLevels.user, icon:'group-work', description:'' },
-				{ route: 'bookstore',  	   name: 'bookstore', 	 moduleId: './modules/bookstore/bookstore', 	      nav: true, title:'Bookstore', access: accessLevels.user, icon:'book', description:'' },
+				{ route: 'bookstore',  	   name: 'bookstore',  moduleId: './modules/bookstore/bookstore', 	      nav: true, title:'Bookstore', access: accessLevels.user, icon:'book', description:'' },
 				// //{ route: 'converters',     name: 'converters', 	 moduleId: './converters/converters', 	  nav: true, title:'Converters', auth:true, icon:'users', description:'' },
-				{ route: 'users',          name: 'users',        moduleId: 'modules/users/users',   nav: true, title:'Github Users', access: accessLevels.user, icon:'github', description:''  },
+				{ route: 'users',          name: 'users',      moduleId: 'modules/users/users',   nav: true, title:'Github Users', access: accessLevels.user, icon:'github', description:''  },
 				{ route: 'signup',         name: 'signup', 		 moduleId: 'account/signup',       	  nav: false, title:'Signup', icon:'', description:'pencil-square' },
 				{ route: 'login',          name: 'login', 		 moduleId: 'account/login',       	  nav: false, title:'Login', icon:'', description:'' },
 				{ route: 'logout',         name: 'logout', 		 moduleId: 'account/logout',       	  nav: false, title:'Logout', icon:'', description:'' },
 				{ route: 'profile',        name: 'profile', 	 moduleId: 'profile/profile',         nav: false, title:'Profile', icon:'male', description:'', access: accessLevels.user },
-				{ route: 'admin',        name: 'admin', 	 moduleId: 'admin/admin',           			nav: true, title:'Admin', icon:'shield-security', description:'', access: accessLevels.admin }
+				{ route: 'admin',        	 name: 'admin', 	   moduleId: 'admin/admin',           			nav: true, title:'Admin', icon:'shield-security', description:'', access: accessLevels.admin }
   		]);
 			config.mapUnknownRoutes(instruction => {
 			   self.router.navigate('login');
