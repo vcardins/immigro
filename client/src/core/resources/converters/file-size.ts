cdb2b05c79773abc16b927ce87ac52bb8e03d5f0
Victor Cardins <victor@results.com>
2015-09-19 00:21:49 -0300

    Added new value converters

---
+++ client/src/core/resources/converters/file-size.ts
@@ -0,0 +1,28 @@
+
+export class FileSizeValueConverter {
+
+  toView(bytes:number, precision:number = 2) {
+    let units = [
+      'bytes',
+      'KB',
+      'MB',
+      'GB',
+      'TB',
+      'PB'
+    ];
+
+    if ( isNaN( parseFloat( bytes )) || ! isFinite( bytes ) ) {
+      return '?';
+    }
+
+    let unit = 0;
+
+    while ( bytes >= 1024 ) {
+      bytes /= 1024;
+      unit ++;
+    }
+    let result = bytes.toFixed( + precision ) + ' ' + units[ unit ];
+    return result;
+  }
+  
+}
