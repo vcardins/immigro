4102d3bd3acdebc648940af319096e6e05df950f
Victor Cardins <victor@results.com>
2015-09-19 00:24:56 -0300

    Update resource index

--- client/src/core/resources/index.ts
+++ client/src/core/resources/index.ts
@@ -8,12 +8,20 @@ export function configure(aurelia) {
     `${path}date-format`,
     `${path}markdown`,
     `${path}number-format`,
+    `${path}file-size`,
     `${path}object-to-array`,
     `${path}rgb-to-hex`,
     `${path}sort`,
     `${path}source`,
     `${path}take`,
-    `${path}upper`  
+    `${path}upper`,
+    `${path}date-timesince`,
+    `${path}default-value`,
+    `${path}numeric-filesizeformat`,
+    `${path}object-keys`,
+    `${path}sort-array`,
+    `${path}string-capitalize`,
+    `${path}string-cut`
     //`${pathUtils}blur-image`
   );
 
